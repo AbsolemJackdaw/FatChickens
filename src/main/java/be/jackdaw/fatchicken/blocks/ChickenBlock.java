1380778c0684b29db8fa7d4cfa32faf6b0514c92
Subaraki <robbeenaxel@hotmail.com>
2021-12-18 23:58:38 +0100

    Added common foodblock to handle a helper method for shapes and direction. added pigblock (ribs) to display fatpig cooked food

--- src/main/java/be/jackdaw/fatchicken/blocks/ChickenBlock.java
+++ src/main/java/be/jackdaw/fatchicken/blocks/ChickenBlock.java
@@ -1,37 +1,41 @@
 package be.jackdaw.fatchicken.blocks;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.core.Direction;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class ChickenBlock extends Block {
-    private static final VoxelShape body = Shapes.create(new AABB(0.0625 * 4d, 0.0625D, 0.0625d * 3, 0.0625d * 12, 0.0625d * 6, 0.0625d * 13));
+public class ChickenBlock extends FoodBlock {
     private static final VoxelShape board = Shapes.create(new AABB(0.0625 * 2d, 0.0D, 0.0625d * 3, 0.0625d * 14, 0.0625d * 1, 0.0625d * 14));
-    private static final VoxelShape leg_r = Shapes.create(new AABB(0.0625 * 2, 0.0625 * 2D, 0.0625d * 4, 0.0625d * 4, 0.0625d * 6, 0.0625d * 8));
-    private static final VoxelShape leg_l = Shapes.create(new AABB(0.0625 * 12, 0.0625 * 2D, 0.0625d * 4, 0.0625d * 14, 0.0625d * 6, 0.0625d * 8));
-    private static final VoxelShape chicken = Shapes.or(body, board, leg_r, leg_l);
+
+    private static final VoxelShape body_n_s = Shapes.create(new AABB(0.0625 * 4d, 0.0625D, 0.0625d * 3, 0.0625d * 12, 0.0625d * 6, 0.0625d * 13));
+    private static final VoxelShape body_e_w = Shapes.create(new AABB(0.0625 * 3d, 0.0625D, 0.0625d * 4, 0.0625d * 13, 0.0625d * 6, 0.0625d * 12));
+
+    private static final VoxelShape leg_r_n = Shapes.create(new AABB(0.0625 * 2, 0.0625 * 2D, 0.0625d * 4, 0.0625d * 4, 0.0625d * 6, 0.0625d * 8));
+    private static final VoxelShape leg_r_s = Shapes.create(new AABB(0.0625 * 2, 0.0625 * 2D, 0.0625d * 8, 0.0625d * 4, 0.0625d * 6, 0.0625d * 12));
+
+    private static final VoxelShape leg_l_n = Shapes.create(new AABB(0.0625 * 12, 0.0625 * 2D, 0.0625d * 4, 0.0625d * 14, 0.0625d * 6, 0.0625d * 8));
+    private static final VoxelShape leg_l_s = Shapes.create(new AABB(0.0625 * 12, 0.0625 * 2D, 0.0625d * 8, 0.0625d * 14, 0.0625d * 6, 0.0625d * 12));
+
+    private static final VoxelShape leg_r_e = Shapes.create(new AABB(0.0625 * 8, 0.0625 * 2D, 0.0625d * 2, 0.0625d * 12, 0.0625d * 6, 0.0625d * 4));
+    private static final VoxelShape leg_r_w = Shapes.create(new AABB(0.0625 * 4, 0.0625 * 2D, 0.0625d * 2, 0.0625d * 8, 0.0625d * 6, 0.0625d * 4));
+
+    private static final VoxelShape leg_l_e = Shapes.create(new AABB(0.0625 * 8, 0.0625 * 2D, 0.0625d * 12, 0.0625d * 12, 0.0625d * 6, 0.0625d * 14));
+    private static final VoxelShape leg_l_w = Shapes.create(new AABB(0.0625 * 4, 0.0625 * 2D, 0.0625d * 12, 0.0625d * 8, 0.0625d * 6, 0.0625d * 14));
 
     public ChickenBlock(Properties properties) {
         super(properties);
     }
 
     @Override
-    public VoxelShape getCollisionShape(BlockState p_60572_, BlockGetter p_60573_, BlockPos p_60574_, CollisionContext p_60575_) {
-        return chicken;
-    }
+    public VoxelShape directionalShape(Direction direction) {
 
-    @Override
-    public VoxelShape getVisualShape(BlockState p_60479_, BlockGetter p_60480_, BlockPos p_60481_, CollisionContext p_60482_) {
-        return chicken;
-    }
-
-    @Override
-    public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
-        return chicken;
+        return switch (direction) {
+            case NORTH -> Shapes.or(body_n_s, board, leg_r_n, leg_l_n);
+            case SOUTH -> Shapes.or(body_n_s, board, leg_r_s, leg_l_s);
+            case EAST -> Shapes.or(body_e_w, board, leg_r_e, leg_l_e);
+            case WEST -> Shapes.or(body_e_w, board, leg_r_w, leg_l_w);
+            default -> board;
+        };
     }
 }
