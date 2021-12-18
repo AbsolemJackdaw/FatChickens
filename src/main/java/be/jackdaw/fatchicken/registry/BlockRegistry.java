2075ef2eb09e6bf44b6062b730849c84273d0614
Subaraki <robbeenaxel@hotmail.com>
2021-12-18 23:59:00 +0100

    added fatpig block food added fatpig raw item

--- src/main/java/be/jackdaw/fatchicken/registry/BlockRegistry.java
+++ src/main/java/be/jackdaw/fatchicken/registry/BlockRegistry.java
@@ -2,6 +2,7 @@ package be.jackdaw.fatchicken.registry;
 
 import be.jackdaw.fatchicken.FatChickenMod;
 import be.jackdaw.fatchicken.blocks.ChickenBlock;
+import be.jackdaw.fatchicken.blocks.PigBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -11,5 +12,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class BlockRegistry {
     public static DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, FatChickenMod.MODID);
-    public static RegistryObject<Block> FATCHICKEN = BLOCKS.register(FatChickenMod.MODID, () -> new ChickenBlock(BlockBehaviour.Properties.of(Material.CAKE).noOcclusion().destroyTime(1.0f)));
+    public static RegistryObject<Block> FATCHICKEN = BLOCKS.register("fatchicken", () -> new ChickenBlock(BlockBehaviour.Properties.of(Material.CAKE).noOcclusion().destroyTime(1.0f)));
+    public static RegistryObject<Block> FATPIG = BLOCKS.register("fatpig", () -> new PigBlock(BlockBehaviour.Properties.of(Material.CAKE).noOcclusion().destroyTime(1.0f)));
+
 }
--- src/main/java/be/jackdaw/fatchicken/registry/ItemRegistry.java
+++ src/main/java/be/jackdaw/fatchicken/registry/ItemRegistry.java
@@ -1,6 +1,8 @@
 package be.jackdaw.fatchicken.registry;
 
 import be.jackdaw.fatchicken.FatChickenMod;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -12,8 +14,15 @@ import net.minecraftforge.registries.RegistryObject;
 public class ItemRegistry {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FatChickenMod.MODID);
-    private static final FoodProperties chicken = new FoodProperties.Builder().nutrition(10).saturationMod(1.0F).meat().build();
+    private static final FoodProperties chicken = new FoodProperties.Builder().nutrition(10).saturationMod(1.0F).meat().effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 50, 1), 1f).build();
+    private static final FoodProperties pig = new FoodProperties.Builder().nutrition(18).saturationMod(1.6F).meat().effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 50, 1), 1f).build();
+
     public static RegistryObject<Item> COOKED_FATCHICKEN = ITEMS.register("fatchicken", () -> new BlockItem(BlockRegistry.FATCHICKEN.get(), new Item.Properties().
             food(chicken).tab(CreativeModeTab.TAB_FOOD)));
     public static RegistryObject<Item> FATCHICKEN = ITEMS.register("fatchicken_raw", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static RegistryObject<Item> COOKED_FATPIG = ITEMS.register("fatpig", () -> new BlockItem(BlockRegistry.FATPIG.get(), new Item.Properties().
+            food(pig).tab(CreativeModeTab.TAB_FOOD)));
+    public static RegistryObject<Item> FATPIG = ITEMS.register("fatpig_raw", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
 }
