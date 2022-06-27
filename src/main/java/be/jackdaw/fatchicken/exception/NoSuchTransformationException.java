a83120a42ad024fdcdbb843108d4a2f22c7eb84a
Subaraki <robbeenaxel@hotmail.com>
2022-06-27 15:39:32 +0200

    update to 1.18.2

--- src/main/java/be/jackdaw/fatchicken/exception/NoSuchTransformationException.java
+++
@@ -1,7 +0,0 @@
-package be.jackdaw.fatchicken.exception;
-
-public class NoSuchTransformationException extends RuntimeException {
-    public NoSuchTransformationException(String type) {
-        super("No Transformation has been found for the entitytype " + type);
-    }
-}
