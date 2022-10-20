683ed6e4e7a0a8e78bd68aa1ab92e49ed7b7b7b6
Subaraki <robbeenaxel@hotmail.com>
2022-10-20 11:04:06 +0200

    fix server crash on forge. add client tag to event loader for model registry.

--- Forge/src/main/java/jackdaw/fatchicken/event/ModelLoadingEvent.java
+++ Forge/src/main/java/jackdaw/fatchicken/event/ModelLoadingEvent.java
@@ -1,12 +1,13 @@
 package jackdaw.fatchicken.event;
 
 import jackdaw.fatchicken.CommonMod;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
-@Mod.EventBusSubscriber(modid = CommonMod.MODID, bus = Bus.MOD)
+@Mod.EventBusSubscriber(modid = CommonMod.MODID, value = Dist.CLIENT, bus = Bus.MOD)
 public class ModelLoadingEvent {
 
     @SubscribeEvent
